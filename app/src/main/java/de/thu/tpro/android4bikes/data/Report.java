fea8ab1eef2767b843072825f9e8c9f3db9cb0e8
LucaTonak <luca.tonak@gmail.com>
2020-03-23 17:50:09 +0100

    Renamed Meldung to Report

--- app/src/main/java/de/thu/tpro/android4bikes/data/Report.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/Report.java
@@ -4,7 +4,7 @@ import org.json.JSONObject;
 
 import de.thu.tpro.android4bikes.database.Content;
 
-public class Meldung implements Content {
+public class Report implements Content {
     @Override
     public JSONObject getJsonRepresentation() {
         return null;
