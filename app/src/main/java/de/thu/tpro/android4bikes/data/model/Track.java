cf2ab78325cad656ab6ec3fe2574f510f74a307b
LucaTonak <luca.tonak@gmail.com>
2020-03-23 17:33:49 +0100

    Extended Track by attributes Added constructors to Track Added getter and setter to Track

--- app/src/main/java/de/thu/tpro/android4bikes/data/model/Track.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/model/Track.java
@@ -1,4 +1,64 @@
 package de.thu.tpro.android4bikes.data.model;
 
+import java.util.List;
+
+import de.thu.tpro.android4bikes.data.Position;
+
 public class Track {
+    private long author;
+    private List<Rating> ratings;
+    private String name;
+    private String description;
+    private List<Position> track;
+
+    public Track() {
+    }
+
+    public Track(long author, List<Rating> ratings, String name, String description, List<Position> track) {
+        this.author = author;
+        this.ratings = ratings;
+        this.name = name;
+        this.description = description;
+        this.track = track;
+    }
+
+    public long getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(long author) {
+        this.author = author;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Position> getTrack() {
+        return track;
+    }
+
+    public void setTrack(List<Position> track) {
+        this.track = track;
+    }
 }
