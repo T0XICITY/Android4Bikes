593031891da03991d3bc97d88c0c32549e4cf4fa
LucaTonak <luca.tonak@gmail.com>
2020-03-23 17:35:37 +0100

    Extended Profile by attributes Added constructors to Profile Added getter and setter to Profile

--- app/src/main/java/de/thu/tpro/android4bikes/data/model/Profil.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/model/Profil.java
@@ -1,4 +1,40 @@
 package de.thu.tpro.android4bikes.data.model;
 
 public class Profil {
+    private String firstName;
+    private String familyName;
+    private long firebaseID;
+
+    public Profil() {
+    }
+
+    public Profil(String firstName, String familyName, long firebaseID) {
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.firebaseID = firebaseID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public long getFirebaseID() {
+        return firebaseID;
+    }
+
+    public void setFirebaseID(long firebaseID) {
+        this.firebaseID = firebaseID;
+    }
 }
