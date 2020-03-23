cce042d2220ae80778220577db85548706e43045
LucaTonak <luca.tonak@gmail.com>
2020-03-23 17:38:29 +0100

    Extended BikeRack by attributes Added constructors to BikeRack Added getter and setter to BikeRack

--- app/src/main/java/de/thu/tpro/android4bikes/data/model/BikeRack.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/model/BikeRack.java
@@ -1,4 +1,62 @@
 package de.thu.tpro.android4bikes.data.model;
 
+import de.thu.tpro.android4bikes.data.Position;
+
 public class BikeRack {
+    private Position position;
+    private String name;
+    private int capacity;
+    private boolean hasBikeCharging;
+    private boolean isCovered;
+
+    public BikeRack() {
+    }
+
+    public BikeRack(Position position, String name, int capacity, boolean hasBikeCharging, boolean isCovered) {
+        this.position = position;
+        this.name = name;
+        this.capacity = capacity;
+        this.hasBikeCharging = hasBikeCharging;
+        this.isCovered = isCovered;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean hasBikeCharging() {
+        return hasBikeCharging;
+    }
+
+    public void setHasBikeCharging(boolean haseBikeCharging) {
+        this.hasBikeCharging = haseBikeCharging;
+    }
+
+    public boolean isCovered() {
+        return isCovered;
+    }
+
+    public void setCovered(boolean covered) {
+        isCovered = covered;
+    }
 }
