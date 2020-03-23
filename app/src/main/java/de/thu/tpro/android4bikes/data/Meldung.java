517749a47a9fbd23a1852904d9e5d769b8a35f08
LucaTonak <luca.tonak@gmail.com>
2020-03-23 17:48:33 +0100

    Implemented necessary methods in Position and Meldung

--- app/src/main/java/de/thu/tpro/android4bikes/data/Meldung.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/Meldung.java
@@ -1,6 +1,12 @@
 package de.thu.tpro.android4bikes.data;
 
+import org.json.JSONObject;
+
 import de.thu.tpro.android4bikes.database.Content;
 
 public class Meldung implements Content {
+    @Override
+    public JSONObject getJsonRepresentation() {
+        return null;
+    }
 }
