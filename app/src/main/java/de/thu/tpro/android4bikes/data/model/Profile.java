53ded3336bcd3e2705a844475ef9c87a60e7195a
LucaTonak <luca.tonak@gmail.com>
2020-03-24 11:01:47 +0100

    Added Color to Profile

--- app/src/main/java/de/thu/tpro/android4bikes/data/model/Profile.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/model/Profile.java
@@ -1,17 +1,29 @@
 package de.thu.tpro.android4bikes.data.model;
 
+import de.thu.tpro.android4bikes.util.Android4BikeColor;
+
 public class Profile {
     private String firstName;
     private String familyName;
     private long firebaseID;
+    private Android4BikeColor color;
 
     public Profile() {
     }
 
-    public Profile(String firstName, String familyName, long firebaseID) {
+    public Profile(String firstName, String familyName, long firebaseID,Android4BikeColor color) {
         this.firstName = firstName;
         this.familyName = familyName;
         this.firebaseID = firebaseID;
+        this.color = color;
+    }
+
+    public Android4BikeColor getColor() {
+        return color;
+    }
+
+    public void setColor(Android4BikeColor color) {
+        this.color = color;
     }
 
     public String getFirstName() {
