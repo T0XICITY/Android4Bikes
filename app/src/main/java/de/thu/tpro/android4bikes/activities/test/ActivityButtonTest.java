094be3ad4d313ff02c9d5249c693efe321fdb855
LucaTonak <luca.tonak@gmail.com>
2020-03-24 10:14:16 +0100

    Added ActivityButtonTest

---
+++ app/src/main/java/de/thu/tpro/android4bikes/activities/test/ActivityButtonTest.java
@@ -0,0 +1,41 @@
+package de.thu.tpro.android4bikes.activities.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
+
+import de.thu.tpro.android4bikes.R;
+import de.thu.tpro.android4bikes.util.BluetoothButtonHandler;
+
+public class ActivityButtonTest extends AppCompatActivity implements Observer {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_test);
+
+        BluetoothButtonHandler.getInstance(getApplicationContext()).addObserver(this);
+
+        // Register new Button? - Call this one
+        // BluetoothButtonHandler.getInstance(getApplicationContext()).detectButton();
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return BluetoothButtonHandler.getInstance(getApplicationContext()).handleKeyEvent(event);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof BluetoothButtonHandler){
+            Toast.makeText(getApplicationContext(),"Gefahr!",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
