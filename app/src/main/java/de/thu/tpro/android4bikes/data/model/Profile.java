1f83d78c352bae8ca80173daafbefffca6840091
LucaTonak <luca.tonak@gmail.com>
2020-03-24 11:24:55 +0100

    Extended Profile by Level, Achievement, overallDistance

--- app/src/main/java/de/thu/tpro/android4bikes/data/model/Profile.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/model/Profile.java
@@ -1,5 +1,8 @@
 package de.thu.tpro.android4bikes.data.model;
 
+import java.util.List;
+
+import de.thu.tpro.android4bikes.data.Achievement;
 import de.thu.tpro.android4bikes.util.Android4BikesColor;
 
 public class Profile {
@@ -7,15 +10,45 @@ public class Profile {
     private String familyName;
     private long firebaseID;
     private Android4BikesColor color;
+    private int overallDistance;
+    private Achievement.Level level;
+    private List<Achievement> achievements;
 
     public Profile() {
     }
 
-    public Profile(String firstName, String familyName, long firebaseID, Android4BikesColor color) {
+    public Profile(String firstName, String familyName, long firebaseID, Android4BikesColor color, int overallDistance, Achievement.Level level, List<Achievement> achievements) {
         this.firstName = firstName;
         this.familyName = familyName;
         this.firebaseID = firebaseID;
         this.color = color;
+        this.overallDistance = overallDistance;
+        this.level = level;
+        this.achievements = achievements;
+    }
+
+    public int getOverallDistance() {
+        return overallDistance;
+    }
+
+    public void setOverallDistance(int overallDistance) {
+        this.overallDistance = overallDistance;
+    }
+
+    public Achievement.Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Achievement.Level level) {
+        this.level = level;
+    }
+
+    public List<Achievement> getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(List<Achievement> achievements) {
+        this.achievements = achievements;
     }
 
     public Android4BikesColor getColor() {
