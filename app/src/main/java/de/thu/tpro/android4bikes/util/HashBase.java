b7c2a3cc0ffd0ff4a829e4f12e40ce9eb06356dc
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-03-24 09:53:03 +0100

    Implementierung der Klasse "HashBase"
    Klasse HashBase, sowie deren Test erstellen.

--- app/src/main/java/de/thu/tpro/android4bikes/util/HashBase.java
+++ app/src/main/java/de/thu/tpro/android4bikes/util/HashBase.java
@@ -1,4 +1,21 @@
 package de.thu.tpro.android4bikes.util;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
+
 public class HashBase {
+    /**
+     * calculates the sha256-Hash of a given string
+     *
+     * @param input string
+     * @return sha256 hash value in hexadecimal representation as a string
+     */
+    public static String calcSha256HashAsHexString(String input) {
+        //compare: https://www.baeldung.com/sha-256-hashing-java
+        String sha256hex = Hashing.sha256()
+                .hashString(input, StandardCharsets.UTF_8)
+                .toString();
+        return sha256hex;
+    }
 }
