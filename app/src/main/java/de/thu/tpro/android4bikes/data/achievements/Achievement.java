30eccb263e999d74c44a3b5cb8d6216ac01d86ed
LucaTonak <56797744+LucaTonak@users.noreply.github.com>
2020-03-24 15:56:25 +0100

    Added interface Content to Achivement

--- app/src/main/java/de/thu/tpro/android4bikes/data/achievements/Achievement.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/achievements/Achievement.java
@@ -1,6 +1,8 @@
 package de.thu.tpro.android4bikes.data.achievements;
 
-public abstract class Achievement {
+import de.thu.tpro.android4bikes.database.Content;
+
+public abstract class Achievement implements Content {
 
 
     public enum Level{
