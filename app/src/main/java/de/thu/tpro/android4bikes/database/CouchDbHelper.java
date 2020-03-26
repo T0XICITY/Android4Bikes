cc58a044849abf6c67b45726c7a902cab69cd39e
LucaTonak <56797744+LucaTonak@users.noreply.github.com>
2020-03-26 09:33:03 +0100

    Added necessary Methods

--- app/src/main/java/de/thu/tpro/android4bikes/database/CouchDbHelper.java
+++ app/src/main/java/de/thu/tpro/android4bikes/database/CouchDbHelper.java
@@ -1,5 +1,31 @@
 package de.thu.tpro.android4bikes.database;
 
+import de.thu.tpro.android4bikes.data.model.BikeRack;
+import de.thu.tpro.android4bikes.data.model.HazardAlert;
+import de.thu.tpro.android4bikes.data.model.Position;
+import de.thu.tpro.android4bikes.data.model.Profile;
+import de.thu.tpro.android4bikes.data.model.Track;
+
 public class CouchDbHelper {
     private CouchDB couchDB;
+
+    public CouchDbHelper() {
+        couchDB = new CouchDB();
+    }
+
+    public BikeRack getBikeRack(Position position) {
+        return null;
+    }
+
+    public Profile getProfile(long firebaseID) {
+        return null;
+    }
+
+    public Track getTrack(long trackID) {
+        return null;
+    }
+
+    public HazardAlert getHazardAlert() {
+        return null;
+    }
 }
