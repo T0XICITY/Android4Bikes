92e59ff45eb05bd5fdcb6130b2ad10c7595404c8
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-03-28 12:44:58 +0100

    Added constants regarding the collections contained by our FireStore

--- app/src/main/java/de/thu/tpro/android4bikes/firebase/FirebaseConnection.java
+++ app/src/main/java/de/thu/tpro/android4bikes/firebase/FirebaseConnection.java
@@ -8,14 +8,34 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import de.thu.tpro.android4bikes.data.model.BikeRack;
 import de.thu.tpro.android4bikes.data.model.Profile;
 import de.thu.tpro.android4bikes.util.FirebaseHelper;
 
 import static android.content.ContentValues.TAG;
 
 public class FirebaseConnection {
-    public enum COLLECTIONS{
+    public enum ConstantsFirebase{
+        COLLECTION_USERS("users"),
+        COLLECTION_UTILIZATION("utilization"),
+        COLLECTION_RAWGEOPOS("rawgeopos"),
+        COLLECTION_COARSED_GEOPOS("coarsedgeopos"),
+        COLLECTION_TRACKS("tracks"),
+        COLLECTION_RATINGS("ratings"),
+        COLLECTION_BIKERACKS("bikeracks"),
+        COLLECTION_OFFICIAL_BIKERACKS("officialbikeracks"),
+        COLLECTION_HAZARDS("hazards"),
+        COLLECTION_OFFICIAL_HAZARDS("officialhazards");
 
+        private String type;
+
+        ConstantsFirebase(String type) {
+            this.type = type;
+        }
+
+        public String toString() {
+            return type;
+        }
     }
 
     public static void addProfileToFirestore(Profile profile) {
@@ -34,6 +54,10 @@ public class FirebaseConnection {
                         Log.w(TAG, "Error adding Profile" + profile.getFamilyName(), e);
                     }
                 });
+    }
+
+    public static void addBikeRackToFireStore(BikeRack bikerack){
+
     }
 
     public void updateToken() {
