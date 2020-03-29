d3ac220fa118a37cec1ff2aa60f82e998652c639
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-03-29 15:41:34 +0200

    Updated interface regarding the CouchDBHelper

--- app/src/main/java/de/thu/tpro/android4bikes/database/CouchDbHelper.java
+++ app/src/main/java/de/thu/tpro/android4bikes/database/CouchDbHelper.java
@@ -2,6 +2,8 @@ package de.thu.tpro.android4bikes.database;
 
 import com.google.firebase.firestore.GeoPoint;
 
+import java.util.List;
+
 import de.thu.tpro.android4bikes.data.model.BikeRack;
 import de.thu.tpro.android4bikes.data.model.HazardAlert;
 import de.thu.tpro.android4bikes.data.model.Profile;
@@ -14,14 +16,65 @@ public class CouchDbHelper implements Android4BikesDatabaseHelper {
         couchDB = new CouchDB();
     }
 
+
     @Override
-    public BikeRack getBikeRack(GeoPoint geoPoint) {
+    public List<BikeRack> readBikeRack(String postcode) {
         return null;
     }
 
     @Override
-    public Profile getProfile(long firebaseID) {
+    public void saveBikeRack(BikeRack bikeRack) {
+
+    }
+
+    @Override
+    public void updateBikeRack(BikeRack bikeRack) {
+
+    }
+
+    @Override
+    public void deleteBikeRack(BikeRack bikeRack) {
+
+    }
+
+    @Override
+    public List<HazardAlert> readHazardAlert(String postcode) {
         return null;
+    }
+
+    @Override
+    public void saveHazardAlert(HazardAlert hazardAlert) {
+
+    }
+
+    @Override
+    public void updateHazardAlert(HazardAlert hazardAlert) {
+
+    }
+
+    @Override
+    public void deleteHazardAlert(HazardAlert hazardAlert) {
+
+    }
+
+    @Override
+    public Profile readProfile(String firebaseAccountID) {
+        return null;
+    }
+
+    @Override
+    public void saveProfile(Profile profile) {
+
+    }
+
+    @Override
+    public void updateProfile(Profile profile) {
+
+    }
+
+    @Override
+    public void deleteProfile(Profile profile) {
+
     }
 
     @Override
@@ -29,8 +82,5 @@ public class CouchDbHelper implements Android4BikesDatabaseHelper {
         return null;
     }
 
-    @Override
-    public HazardAlert getHazardAlert() {
-        return null;
-    }
+
 }
