098196540aad5a109f4c1ef9039ea8966dc5c224
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-03-29 12:19:15 +0200

    Unit test for invalid positions regarding the class "GeoLocationHelper"

--- app/src/androidTest/java/de/thu/tpro/android4bikes/util/GeoLocationHelperTest.java
+++ app/src/androidTest/java/de/thu/tpro/android4bikes/util/GeoLocationHelperTest.java
@@ -10,6 +10,7 @@ import de.thu.tpro.android4bikes.data.model.Position;
 import static org.junit.Assert.*;
 
 public class GeoLocationHelperTest {
+
     @BeforeClass
     public static void setUp(){
         GlobalContext.setContext(ApplicationProvider.getApplicationContext());
@@ -21,7 +22,14 @@ public class GeoLocationHelperTest {
         String postcode_technische_hochschule_ulm_real = "89075";
         String postcode_technische_hochschule_ulm_calculated = GeoLocationHelper.convertPositionToPostcode(position_technische_hochschule_ulm);
 
-
         assertEquals(postcode_technische_hochschule_ulm_real, postcode_technische_hochschule_ulm_calculated);
+    }
+
+    @Test
+    public void convertInvalidPositionToPosition(){
+        Position position_somalia_desert = new Position(48.408880,9.997507);
+        String invalid_postcode = GeoLocationHelper.convertPositionToPostcode(position_somalia_desert);
+
+        assertEquals(null, invalid_postcode);
     }
 }
