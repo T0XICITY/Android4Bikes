16d4c7f1e2bd090e9d0241f7082bef0437301993
LucaTonak <56797744+LucaTonak@users.noreply.github.com>
2020-03-30 09:03:12 +0200

    Unittest for basic functionality

--- app/src/test/java/de/thu/tpro/android4bikes/services/WeatherManagerOpenWeatherTest.java
+++ app/src/test/java/de/thu/tpro/android4bikes/services/WeatherManagerOpenWeatherTest.java
@@ -1,7 +1,25 @@
 package de.thu.tpro.android4bikes.services;
 
+import org.junit.Test;
+
+import de.thu.tpro.android4bikes.services.weatherData.openWeather.OpenWeatherObject;
+
 import static org.junit.Assert.*;
 
 public class WeatherManagerOpenWeatherTest {
+
+    @Test
+    public void testBasicFunctionality(){
+        WeatherManagerOpenWeather manager = new WeatherManagerOpenWeather();
+        OpenWeatherObject weatherObject = manager.createOpenWeatherObject(48.3,9.8333);
+
+        assertNotNull(weatherObject);
+        assertNotNull(weatherObject.getList());
+
+        //weatherObject.getList().forEach(entry -> System.out.println(entry.getDtTxt() + " : " + entry.getMain().getTemp()));
+
+        assertEquals("2020-03-30 09:00:00", weatherObject.getList().get(0).getDtTxt());
+        assertEquals(1.65,(double)weatherObject.getList().get(0).getMain().getTemp(),0.0001);
+    }
 
 }
