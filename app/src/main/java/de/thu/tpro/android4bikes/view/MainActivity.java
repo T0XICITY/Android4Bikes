2e082b172106875f9241cbd04cd4d695089790b0
aliwer07 <aboydadaev@gmail.com>
2020-04-01 11:11:11 +0200

    +++ Initial commit for Google Map API

--- app/src/main/java/de/thu/tpro/android4bikes/view/MainActivity.java
+++ app/src/main/java/de/thu/tpro/android4bikes/view/MainActivity.java
@@ -1,33 +1,48 @@
 package de.thu.tpro.android4bikes.view;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import de.thu.tpro.android4bikes.R;
+import de.thu.tpro.android4bikes.util.GlobalContext;
 import de.thu.tpro.android4bikes.view.info.FragmentInfoMode;
-import de.thu.tpro.android4bikes.view.menu.roadsideAssistance.FragmentRoadsideAssistance;
-import de.thu.tpro.android4bikes.view.menu.createTrack.FragmentCreateTrack;
 import de.thu.tpro.android4bikes.view.menu.roadsideAssistance.FragmentRoadsideAssistance;
 
 /**
  * @author stlutz
  * This activity acts as a container for all fragments
  */
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
 
     private static final String LOG_TAG = "MainActivity";
     /**
@@ -39,6 +54,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ImageButton btn_community;
     private DrawerLayout dLayout;
     private NavigationView drawer;
+    private static final int REQUEST_CODE = 100;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 102;
+    private Location currentLocation;
+    private GoogleMap googleMap;
+    private FusedLocationProviderClient flpc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +66,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         initNavigationDrawer();
         initBottomNavigation();
+
+        GlobalContext.setContext(getApplicationContext());
+        flpc = LocationServices.getFusedLocationProviderClient(this);
+        fetchLastLocation();
+
+
         initFAB();
     }
 
@@ -80,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         updateFragment();
         return true;
     }
+
     /**
      * Initiates the BottomAppBar and set listeners to nav buttons
      */
@@ -133,6 +160,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
     }
+
     /**
      * Creates a Snackbar to test the floating action button
      */
@@ -145,6 +173,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      */
     private void updateFragment() {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
+    }
+
+    /**
+     * Fetch the last location
+     */
+    private void fetchLastLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = flpc.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() +
+                            " " + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+
+                    // set callback listener on Google Map ready
+                    mapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
+    }
+
+    /**
+     * Request for the permission
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLastLocation();
+                }
+                break;
+        }
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("My current location");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions);
     }
 
 
