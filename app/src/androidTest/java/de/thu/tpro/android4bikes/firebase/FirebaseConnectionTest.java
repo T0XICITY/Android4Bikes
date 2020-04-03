7aee99baf10b7b35a3c98d1e5c4542729bb49a52
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-04-03 17:32:07 +0200

    AN-199: Added JavaDoc to the test that submits a BikeRack

--- app/src/androidTest/java/de/thu/tpro/android4bikes/firebase/FirebaseConnectionTest.java
+++ app/src/androidTest/java/de/thu/tpro/android4bikes/firebase/FirebaseConnectionTest.java
@@ -19,6 +19,11 @@ import de.thu.tpro.android4bikes.util.GlobalContext;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests if the communication between {@link com.google.firebase.firestore.FirebaseFirestore} and the application
+ * works correctly. It tests especially if the objects (e.g {@link BikeRack}) involved in the communication are
+ * exchanged successfully and in the correct format.
+ */
 public class FirebaseConnectionTest {
     private static FirebaseConnection firebaseConnection;
     private static CouchDBHelper couchDBHelper;
@@ -37,7 +42,8 @@ public class FirebaseConnectionTest {
         Profile profile_kostas = this.createProfile();
 
         firebaseConnection.storeProfileToFireStoreAndLocalDB(profile_kostas);
-        //wait a few seconds
+
+        //wait a few seconds because of the asynchronous process of storing data to FireBase
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
@@ -46,22 +52,47 @@ public class FirebaseConnectionTest {
 
         Profile read_profile = couchDBHelper.readProfile(profile_kostas.getGoogleID());
         assertEquals(profile_kostas, read_profile);
+
+
+        //TODO: Remove comment
+        //firebaseConnection.deleteProfileFromFireStoreAndLocalDB(profile_kostas.getGoogleID());
     }
 
+    /**
+     * 1. Generates a {@link de.thu.tpro.android4bikes.data.model.BikeRack} 'THU' that is located in Ulm
+     * 2. Stores the same {@link de.thu.tpro.android4bikes.data.model.BikeRack} 'THU' three times to the {@link com.google.firebase.firestore.FirebaseFirestore}
+     * 3. Reads all official exemplars of the class {@link de.thu.tpro.android4bikes.data.model.BikeRack} with the postal code '89075' from the {@link com.google.firebase.firestore.FirebaseFirestore} and stores them to the {@link com.couchbase.lite.CouchbaseLite}
+     * 4. Reads all stored {@link de.thu.tpro.android4bikes.data.model.BikeRack} with the postal code '89075' from the {@link com.couchbase.lite.CouchbaseLite}
+     * <p>
+     * Requirements for passing this test:
+     * -the {@link de.thu.tpro.android4bikes.data.model.BikeRack} 'THU' should be in the list of read {@link de.thu.tpro.android4bikes.data.model.BikeRack}
+     */
     @Test
     public void storeBikeRackInFireStoreAndLocalDB() {
-        BikeRack bikeRack_THU = new BikeRack(
-                "pfo4eIrvzrI0m363KF0K", new Position(9.997507, 48.408880), "THUBikeRack", BikeRack.ConstantsCapacity.SMALL,
-                false, true, false
-        );
+        //generate a bike rack
+        BikeRack bikeRack_THU = generateTHUBikeRack();
 
-        firebaseConnection.submitBikeRackToFireStoreAndLocalDB(bikeRack_THU);
+        //store the same bike rack three times in the FireStore. After the third occurence
+        //of this bike rack in the FireStore it should be an official bike rack. Only
+        //official bike racks can be read!!!
+        for (int i = 0; i < 3; ++i) {
+            firebaseConnection.submitBikeRackToFireStoreAndLocalDB(bikeRack_THU);
+        }
+
+        //wait a few seconds because of the asynchronous process of storing data to FireBase
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        List<BikeRack> bikeRacks_with_postcode_89075 = couchDBHelper.readBikeRacks(bikeRack_THU.getFirebaseID());
+
+        //read official bike racks from couch db and store them to the local db
+        firebaseConnection.readBikeRacksFromFireStoreAndStoreItToLocalDB(bikeRack_THU.getPostcode());
+
+        //read all bike racks from the local db with the postcode '89075'
+        List<BikeRack> bikeRacks_with_postcode_89075 = couchDBHelper.readBikeRacks(bikeRack_THU.getPostcode());
+
+        //the just stored bike rack has to be contained in the list of official bike racks
         assertTrue(bikeRacks_with_postcode_89075.contains(bikeRack_THU));
     }
 
@@ -80,6 +111,11 @@ public class FirebaseConnectionTest {
     public void updateToken() {
     }
 
+    /**
+     * generates a new instance of the class {@link de.thu.tpro.android4bikes.data.model.Profile} for test purposes
+     *
+     * @return instance of the class {@link de.thu.tpro.android4bikes.data.model.Profile}
+     */
     private Profile createProfile() {
         List<Achievement> achievements = new ArrayList<>();
         achievements.add(new KmAchievement("First Mile", 1, 1, 1, 2));
@@ -87,5 +123,19 @@ public class FirebaseConnectionTest {
 
         Profile profile = new Profile("Kostas", "Kostidis", "00x15dxxx", 10, 250, achievements);
         return profile;
+    }
+
+    /**
+     * generates a new instance of the class {@link de.thu.tpro.android4bikes.data.model.BikeRack} for test purposes
+     *
+     * @return instance of the class {@link de.thu.tpro.android4bikes.data.model.BikeRack}
+     */
+    private BikeRack generateTHUBikeRack() {
+        //create new BikeRack
+        BikeRack bikeRack_THU = new BikeRack(
+                "pfo4eIrvzrI0m363KF0K", new Position(9.997507, 48.408880), "THUBikeRack", BikeRack.ConstantsCapacity.SMALL,
+                false, true, false
+        );
+        return bikeRack_THU;
     }
 }
