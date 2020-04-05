faf309e3856179545a149b54ac384bfeb8a07f79
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-04-05 15:27:25 +0200

    Position-Compressor: Finalised the code and the associated unit-test regarding the class "Position-Compressor". Also added a class JSONHelper for the conversions regarding JSON-Objects.

--- app/src/androidTest/java/de/thu/tpro/android4bikes/util/compression/PositionCompressorTest.java
+++ app/src/androidTest/java/de/thu/tpro/android4bikes/util/compression/PositionCompressorTest.java
@@ -7,15 +7,20 @@ import java.util.List;
 
 import de.thu.tpro.android4bikes.data.model.Position;
 
+import static org.junit.Assert.assertEquals;
+
 public class PositionCompressorTest {
 
     @Test
-    public void compressPositions() {
+    public void compressAndDecompressPositions() {
         List<Position> finegrainedpositions = new ArrayList<>();
         finegrainedpositions.add(new Position(9.997507, 48.408880));
         finegrainedpositions.add(new Position(9.997509, 48.408887));
 
         PositionCompressor positionCompressor = new PositionCompressor();
-        positionCompressor.compressPositions(finegrainedpositions);
+        byte[] compressedPositionList = positionCompressor.compressPositions(finegrainedpositions);
+
+        List<Position> decompressedPositionList = positionCompressor.decompressPositions(compressedPositionList);
+        assertEquals(finegrainedpositions, decompressedPositionList);
     }
 }
