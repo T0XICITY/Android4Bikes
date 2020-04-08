6dccaf3b75fcdbc1b9366fe4256a3064f4a3562d
nektariosNakosTHU <56817747+nektariosNakosTHU@users.noreply.github.com>
2020-04-08 11:56:46 +0200

    AN-220: Created comments regarding the first thoughts about the implementation.

---
+++ app/src/main/java/de/thu/tpro/android4bikes/viewmodel/ViewModelTrack.java
@@ -0,0 +1,8 @@
+package de.thu.tpro.android4bikes.viewmodel;
+
+public class ViewModelTrack {
+    //UI -> Datenhaltung
+    //SUBMITTRACK: Create Track -> ViewModel ruft FireBase an
+    //READTRACK: Liste an Tracks erhalten (PLZ) -> alle tracks in ulm sehen
+
+}
