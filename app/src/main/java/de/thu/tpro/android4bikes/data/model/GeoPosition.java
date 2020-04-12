b6dcaf96a00f4a0c00b4e25c66a5ac509d3cc60d
Fabi98 <fabian.dittrich98@gmail.com>
2020-04-12 13:11:33 +0200

    added test data

--- app/src/main/java/de/thu/tpro/android4bikes/data/model/GeoPosition.java
+++ app/src/main/java/de/thu/tpro/android4bikes/data/model/GeoPosition.java
@@ -2,19 +2,16 @@ package de.thu.tpro.android4bikes.data.model;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import org.imperiumlabs.geofirestore.GeoFirestore;
 import org.imperiumlabs.geofirestore.GeoQuery;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class GeoPosition {
@@ -24,10 +21,11 @@ public class GeoPosition {
     private CollectionReference collectionReference;
     private GeoFirestore geoFirestore;
 
-    public GeoPosition() {
-        collectionReference = FirebaseFirestore.getInstance().collection("mycollection");
+    public GeoPosition(String collectionpath) {
+        collectionReference = FirebaseFirestore.getInstance().collection(collectionpath);
         geoFirestore = new GeoFirestore(collectionReference);
     }
+
 
     public void setLocation(String id, GeoPoint geoPoint) {
         geoFirestore.setLocation(id, geoPoint, exception -> {
@@ -39,44 +37,39 @@ public class GeoPosition {
         });
     }
 
-    public void getLocation(String id) {
-        geoFirestore.getLocation("test667", new GeoFirestore.LocationCallback() {
-            @Override
-            public void onComplete(GeoPoint location, Exception exception) {
-                if (exception == null && location != null) {
-                    Log.d("HALLO WELT!", "The location for this document is" + location.toString());
-                } else {
-                    Log.d("HALLO WELT!", "An error has occurred: " + exception.getMessage());
-                }
+    public void getLocationQuery(GeoPoint geoPoint, double radius) {
+
+        geoFirestore.getAtLocation(geoPoint, radius, (list, e) -> {
+            if (e != null) {
+                Log.d("HALLO WELT", "Error: ");
+                e.printStackTrace();
+            } else {
+                list.forEach(document -> Log.d("HALLO WELT", document.toString()));
             }
+
         });
-
-
     }
 
-    public void geoQuery(GeoPoint geoPoint, int radius) {
-        GeoQuery geoQuery = geoFirestore.queryAtLocation(geoPoint, radius);
-
+    public void geoQuery(GeoPoint geoPoint, double radius_km) {
+        GeoQuery geoQuery = geoFirestore.queryAtLocation(geoPoint, radius_km);
+        Log.d("HALLO WELT", "Km: " + geoQuery.getRadius());
+        Log.d("HALLO WELT", "Meilen?: " + geoQuery.getRadius() * 1.609d);
+        AtomicInteger count = new AtomicInteger(1);
         for (Query query : geoQuery.getQueries()) {
             query.get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                    Log.d("HALLO WELT", document.getId() + " => " + document.getData());
-                                }
-                            } else {
-                                Log.d("HALLO WELT", "Error getting documents: ", task.getException());
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d("HALLO WELT", "Query: " + count.getAndIncrement() + "\tElemente:" + task.getResult().getDocuments().size());
+
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("HALLO WELT", document.getId() + " => " + document.getData());
                             }
+                        } else {
+                            Log.d("HALLO WELT", "Error getting documents: ", task.getException());
                         }
                     });
         }
-
-
-
-
-
     }
 
 }
