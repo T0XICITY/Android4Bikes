f100eed4ece883e6b562766fabe5c2755c01a009
LucaTonak <56797744+LucaTonak@users.noreply.github.com>
2020-04-13 15:15:31 +0200

    AN-255: Created and implemented the Processor class

---
+++ app/src/main/java/de/thu/tpro/android4bikes/util/Processor.java
@@ -0,0 +1,35 @@
+package de.thu.tpro.android4bikes.util;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Processor {
+    private static Processor instance;
+    private ExecutorService executorService;
+
+    private Processor() {
+        /*
+        Der CachedThreadPool bestimmt selbst mit wie vielen Threads er eine Liste von Runnables oder Callables abarbeitet.
+        Er erzeugt bei Bedarf neue Threads oder verwendet auch alte wieder. Threads, die 60 Sekunden lang nicht verwendet wurden, werden aus dem Pool entfernt.
+        Auf diese Weise werden Resourcen gespart und so die Performance verbessert. Der CachedThreadPool ist gut geeignet für viele kurzlebige Prozesse,
+        hier hat er eine bessere Performance als andere ThreadPools.
+        ~ https://www.straub.as/java/threads/cachedthreadpool.html
+        */
+        executorService = Executors.newCachedThreadPool();
+    }
+
+    public static Processor getInstance() {
+        if (instance == null) {
+            instance = new Processor();
+        }
+        return instance;
+    }
+
+    public void startRunnable(Runnable runnable) {
+        executorService.execute(runnable);
+    }
+
+    public void shutdown() {
+        executorService.shutdown();
+    }
+}
