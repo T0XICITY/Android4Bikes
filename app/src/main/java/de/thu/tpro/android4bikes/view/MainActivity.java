93e8dc3bacda8af8861e160f72148706d9f4e0c3
LucaTonak <56797744+LucaTonak@users.noreply.github.com>
2020-04-15 08:13:37 +0200

    Added import

--- app/src/main/java/de/thu/tpro/android4bikes/view/MainActivity.java
+++ app/src/main/java/de/thu/tpro/android4bikes/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
