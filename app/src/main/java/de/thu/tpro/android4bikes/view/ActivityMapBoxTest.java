2d2de565a2fa9ac5afeecdcdd168bae99de21a96
LucaTonak <56797744+LucaTonak@users.noreply.github.com>
2020-04-17 10:36:53 +0200

    Created Testactivity

---
+++ app/src/main/java/de/thu/tpro/android4bikes/view/ActivityMapBoxTest.java
@@ -0,0 +1,16 @@
+package de.thu.tpro.android4bikes.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.thu.tpro.android4bikes.R;
+
+public class ActivityMapBoxTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_box_test);
+    }
+}
