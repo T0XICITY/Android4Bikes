de156eeeb338239d5d11cf6d425e78e33f15a448
Fabi98 <fabian.dittrich98@gmail.com>
2020-04-22 13:07:24 +0200

    added Fabs for weather

--- app/src/main/java/de/thu/tpro/android4bikes/view/driving/FragmentDrivingMode.java
+++ app/src/main/java/de/thu/tpro/android4bikes/view/driving/FragmentDrivingMode.java
@@ -6,6 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
@@ -161,6 +164,25 @@ public class FragmentDrivingMode extends Fragment implements LocationListener, P
 
     }
 
+    public static Bitmap textAsBitmap(String text, float textSize, int textColor) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText(text) + 0.0f); // round
+        int height = (int) (baseline + paint.descent() + 0.0f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(text, 0, baseline, paint);
+        return image;
+    }
+
+    private void initspeedFAB(View view) {
+
+    }
+
     private void initNavFab(View view) {
         navFab = view.findViewById(R.id.start_nav);
         navFab.setOnClickListener(new View.OnClickListener() {
@@ -193,6 +215,8 @@ public class FragmentDrivingMode extends Fragment implements LocationListener, P
                                 @Override
                                 public void onNavigationRunning() {
                                     Log.d("HELLO", String.valueOf(parent.lastSpeed));
+                                    //fab.setImageBitmap(textAsBitmap("OK", 40, Color.WHITE));
+
                                 }
                             })
                             .shouldSimulateRoute(true)
